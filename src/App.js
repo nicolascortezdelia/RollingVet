1b4228de439b12e623dccd3be54e87bbdc2c3de9
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-02-04 16:56:16 -0300

    importo componente Home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/views/Home/Home";
 import Navigation from "./components/layout/Navigation";
 import Footer from "./components/layout/Footer";
-import ProductsTable from "./components/views/ProductsTable/ProductsTable";
+import ProductsTable from "./components/views/ProductTable/ProductTable";
 import ProductCreate from "./components/views/ProductCreate/ProductCreate";
 import ProductEdit from "./components/views/ProductEdit/ProductEdit";
 import Error404 from "./components/views/Error404/Error404";
