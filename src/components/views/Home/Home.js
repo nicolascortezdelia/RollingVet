766576eb6352422cc0458c139fa46d68f5712c98
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-02-11 20:01:53 -0300

    agrego componente Servicios y Planes

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -2,16 +2,21 @@ import React from 'react';
 import HomeCarousel from "./HomeCarousel/HomeCarousel";
 import Planes from "./Planes/Planes";
 import Staff from './Staff/Staff';
+import Servicios from './Servicios/Servicios';
+import Clientes from './Clientes/Clientes';
 
 const Home = () => {
     return (
         <div>
         <HomeCarousel />
       <p className="lead display-1 text-center mb-3 mt-2">"Cuidando a tus mascotas desde hace 70 años"</p>
-      <h1 className="text-center mb-3">Planes</h1>
-      <Planes />
+      <Servicios></Servicios>
       <h1 className="text-center mb-3 mt-3">Nuestros profesionales</h1>
       <Staff /> 
+      <h1 className="text-center mb-3">Planes</h1>
+      <Planes />
+      <Clientes></Clientes>
+
         </div>
     );
 };
