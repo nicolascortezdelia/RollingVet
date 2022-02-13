efb09fda9974a6828e28f80d903e46baa1ab389d
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-02-13 18:36:06 -0300

    modifico imagenes de componente publicidad

--- src/components/views/Home/Publicidad/Publicidad.js
+++ src/components/views/Home/Publicidad/Publicidad.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import {
-    Col,
-    Container,
-    Row,
-    Card,
-    
-  } from "react-bootstrap";
+import React from "react";
+import { Col, Container, Row, Card } from "react-bootstrap";
 
 const Publicidad = () => {
-    return (
-        <Container fluid className="bg-danger mt-4">
-  <Row>
-    <Col sm={12} md={4}>sm=8</Col>
-    <Col sm={12} md={4}>sm=8</Col>
-    <Col sm={12} md={4}>sm=8</Col>
-    
-  </Row>
-</Container>
-    );
+  return (
+    <Container fluid className="bg-light  mt-4">
+      <Row>
+        <Col sm={12} md={4}>
+          <img
+            className="img-responsive"
+            src="https://www.themarkethink.com/wp-content/uploads/2018/06/sobres-pate.jpg"
+            alt=""
+          />
+        </Col>
+
+        <Col sm={12} md={4}>
+          <img
+            src="https://www.themarkethink.com/wp-content/uploads/2018/06/sobres-pate.jpg"
+            alt=""
+          />
+        </Col>
+        <Col sm={12} md={4}>
+          <img
+            src="https://www.themarkethink.com/wp-content/uploads/2018/06/sobres-pate.jpg"
+            alt=""
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Publicidad;
