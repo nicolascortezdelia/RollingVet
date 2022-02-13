52001ef27aa396e823dc7604b8080e71df235086
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-02-13 17:10:30 -0300

    agrego contenido a cards de componente servicios

--- src/components/views/Home/Servicios/Servicios.js
+++ src/components/views/Home/Servicios/Servicios.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 import {
-    Col,
-    Container,
-    Row,
-    Card,
-    ListGroup,
-    ListGroupItem,
-  } from "react-bootstrap";
+  Col,
+  Container,
+  Row,
+  Card,
+  ListGroup,
+  ListGroupItem,
+} from "react-bootstrap";
 
 const Servicios = () => {
-    return (
-        <Container className="mb-3">
-      <Row >
+  return (
+    <Container className="mb-3">
+      <Row>
         <Col sm={12} md={3} className="d-flex justify-content-center mb-4 ">
-          <Card style={{ width: "18rem" }} className="shadow ">
+          <Card style={{ width: "18rem", height: "25rem" }} className="shadow ">
             <Card.Img
               variant="top"
               src="https://vsn.shindiristudio.com/wp-content/uploads/2020/01/Emergency-Vet.jpg"
@@ -21,96 +21,63 @@ const Servicios = () => {
             <Card.Body>
               <Card.Title>Card Title</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Llamanos las 24 hs y te agendamos un examen urgente o respondemos tu consulta de emergencia 
               </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-            <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
+              <Card.Link className="text-c" href="#">Another Link</Card.Link>
             </Card.Body>
           </Card>
         </Col>
         <Col sm={12} md={3} className="d-flex justify-content-center mb-4">
-          <Card style={{ width: "18rem" }} className="shadow">
+          <Card style={{ width: "18rem", height: "25rem" }} className="shadow">
             <Card.Img
               variant="top"
               src="https://vsn.shindiristudio.com/wp-content/uploads/2020/01/About-Us.jpg"
             />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Diagnóstico</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Equipados con la tecnología más sofisticada te garantizamos el
+                mejor diagnóstico para tu mascota.
               </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-            <Card.Body>
               <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
           </Card>
         </Col>
         <Col sm={12} md={3} className="d-flex justify-content-center mb-4">
-        <Card style={{ width: "18rem" }} className="shadow">
+          <Card style={{ width: "18rem", height: "25rem" }} className="shadow">
             <Card.Img
               variant="top"
               src="https://vsn.shindiristudio.com/wp-content/uploads/2020/01/Diagnostics-Surgery.jpg"
             />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Cirugía</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Contamos con los mejores especialistas en cirugías en múltiples
+                especialidades.
               </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-            <Card.Body>
               <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
           </Card>
         </Col>
         <Col sm={12} md={3} className="d-flex justify-content-center mb-4">
-          <Card style={{ width: "18rem" }} className="shadow">
+          <Card style={{ width: "18rem", height: "25rem" }} className="shadow">
             <Card.Img
               variant="top"
               src="https://vsn.shindiristudio.com/wp-content/uploads/2018/07/03-Lecenje.jpg"
             />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Tratamiento Ambulatorio</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Podés dejar a tus mascotas en nuestra clínica para que sean
+                cuidadas por unas horas
               </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-            <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
               <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
           </Card>
         </Col>
       </Row>
     </Container>
-    );
+  );
 };
 
 export default Servicios;
