44663fddcaa2ec691df5dac20ab62cda354ed7c4
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-02-13 17:48:57 -0300

    cambio contenido de cards de Componente Staff

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -12,13 +12,13 @@ const Home = () => {
           <Fade bottom>
         <HomeCarousel />
       <p className="lead display-1 text-center mb-3 mt-2">Rolling Vet</p>
-      <h1 className="text-center mb-3 mt-3">Nuestros Servicios</h1>
+      <h1 className="text-center my-3">Nuestros Servicios</h1>
       <Servicios></Servicios>
       
-      <h1 className="text-center mb-3">Planes</h1>
+      <h1 className="text-center my-3">Planes</h1>
       <Planes />
       {/*aquí va la publicidad*/}
-      <h1 className="text-center mb-3 mt-3">Nuestros profesionales</h1>
+      <h1 className="text-center mb-4 mt-5">Nuestros profesionales</h1>
       <Staff /> 
       <h1 className="text-center mb-3">Testimonios</h1>
       <h3 className="text-center fw-lighter mb-3">Lo que dicen nuestros clientes</h3>
--- src/components/views/Home/Staff/Staff.js
+++ src/components/views/Home/Staff/Staff.js
@@ -15,7 +15,7 @@ const Staff = () => {
     <Container className="mb-3">
       <Row >
         <Col sm={12} md={3} className="d-flex justify-content-center mb-4 ">
-          <Card style={{ width: "18rem" }} className="shadow ">
+        <Card style={{ width: "18rem", height: "23rem" }} className="shadow">
             <Card.Img
               variant="top"
               src="https://images.squarespace-cdn.com/content/v1/53aa5722e4b0ada1a70119f2/1529694594561-7ERUUT9270RZI2R946J1/CORP20180521-Freeman+%284+of+6%29-Edit.jpg?format=2500w"
@@ -26,20 +26,31 @@ const Staff = () => {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </Card.Text>
+              
             </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
+
+          </Card>
+          
+        </Col>
+        <Col sm={12} md={3} className="d-flex justify-content-center mb-4">
+          <Card style={{ width: "18rem", height: "23rem" }} className="shadow">
+            <Card.Img
+              variant="top"
+              src="https://images.squarespace-cdn.com/content/v1/53aa5722e4b0ada1a70119f2/1529694594561-7ERUUT9270RZI2R946J1/CORP20180521-Freeman+%284+of+6%29-Edit.jpg?format=2500w"
+            />
             <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              
             </Card.Body>
+
           </Card>
         </Col>
         <Col sm={12} md={3} className="d-flex justify-content-center mb-4">
-          <Card style={{ width: "18rem" }} className="shadow">
+        <Card style={{ width: "18rem", height: "23rem" }} className="shadow">
             <Card.Img
               variant="top"
               src="https://images.squarespace-cdn.com/content/v1/53aa5722e4b0ada1a70119f2/1529694594561-7ERUUT9270RZI2R946J1/CORP20180521-Freeman+%284+of+6%29-Edit.jpg?format=2500w"
@@ -50,20 +61,13 @@ const Staff = () => {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </Card.Text>
+              
             </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-            <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
+
           </Card>
         </Col>
         <Col sm={12} md={3} className="d-flex justify-content-center mb-4">
-        <Card style={{ width: "18rem" }} className="shadow">
+        <Card style={{ width: "18rem", height: "23rem" }} className="shadow">
             <Card.Img
               variant="top"
               src="https://images.squarespace-cdn.com/content/v1/53aa5722e4b0ada1a70119f2/1529694594561-7ERUUT9270RZI2R946J1/CORP20180521-Freeman+%284+of+6%29-Edit.jpg?format=2500w"
@@ -74,40 +78,9 @@ const Staff = () => {
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </Card.Text>
+              
             </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-            <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col sm={12} md={3} className="d-flex justify-content-center mb-4">
-          <Card style={{ width: "18rem" }} className="shadow">
-            <Card.Img
-              variant="top"
-              src="https://images.squarespace-cdn.com/content/v1/53aa5722e4b0ada1a70119f2/1529694594561-7ERUUT9270RZI2R946J1/CORP20180521-Freeman+%284+of+6%29-Edit.jpg?format=2500w"
-            />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-            <Card.Body>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
+
           </Card>
         </Col>
       </Row>
