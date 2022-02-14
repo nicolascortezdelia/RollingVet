8d54061613d6cc26b755e093d2988355f214d51c
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-02-14 20:15:20 -0300

    agrego imagenes de publicidad

--- src/components/views/Home/Publicidad/Publicidad.js
+++ src/components/views/Home/Publicidad/Publicidad.js
@@ -1,27 +1,30 @@
 import React from "react";
 import { Col, Container, Row, Card } from "react-bootstrap";
+import "./publicidad.css"
 
 const Publicidad = () => {
   return (
     <Container fluid className="bg-light  mt-4">
       <Row>
-        <Col sm={12} md={4}>
+      <Col sm={12} md={4}>
           <img
-            className="img-responsive"
-            src="https://www.themarkethink.com/wp-content/uploads/2018/06/sobres-pate.jpg"
+          className="img-size"
+            src="https://cdn.royalcanin-weshare-online.io/1GnLpWYBG95Xk-RBFd3d/v1/puppy-labrador-kitten-british-shorthair-b-w-brand-emblematic"
             alt=""
           />
         </Col>
 
         <Col sm={12} md={4}>
           <img
-            src="https://www.themarkethink.com/wp-content/uploads/2018/06/sobres-pate.jpg"
+          className="img-size"
+            src="https://cdn.royalcanin-weshare-online.io/lSEua2QBaxEApS7L_vuK/v2/logo-rc-dms-2016-c"
             alt=""
           />
         </Col>
         <Col sm={12} md={4}>
           <img
-            src="https://www.themarkethink.com/wp-content/uploads/2018/06/sobres-pate.jpg"
+          className="img-size"
+            src="https://i.ytimg.com/vi/A4SN8mTgrd4/maxresdefault.jpg"
             alt=""
           />
         </Col>
