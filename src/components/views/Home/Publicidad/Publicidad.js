140234e9c5768eea759e5579156d67a5c61e8569
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-02-14 21:31:16 -0300

    agrego display none a imagenes

--- src/components/views/Home/Publicidad/Publicidad.js
+++ src/components/views/Home/Publicidad/Publicidad.js
@@ -5,8 +5,8 @@ import "./publicidad.css"
 const Publicidad = () => {
   return (
     <Container fluid className="bg-light  mt-4">
-      <Row>
-      <Col sm={12} md={4}>
+      <Row >
+      <Col sm={12} md={4} className="d-flex justify-content-center d-none d-md-block">
           <img
           className="img-size"
             src="https://cdn.royalcanin-weshare-online.io/1GnLpWYBG95Xk-RBFd3d/v1/puppy-labrador-kitten-british-shorthair-b-w-brand-emblematic"
@@ -14,14 +14,14 @@ const Publicidad = () => {
           />
         </Col>
 
-        <Col sm={12} md={4}>
+        <Col sm={12} md={4} className="d-flex justify-content-center img-responsive">
           <img
-          className="img-size"
+          className="img-size "
             src="https://cdn.royalcanin-weshare-online.io/lSEua2QBaxEApS7L_vuK/v2/logo-rc-dms-2016-c"
             alt=""
           />
         </Col>
-        <Col sm={12} md={4}>
+        <Col sm={12} md={4} className="d-flex justify-content-center d-none d-md-block">
           <img
           className="img-size"
             src="https://i.ytimg.com/vi/A4SN8mTgrd4/maxresdefault.jpg"
