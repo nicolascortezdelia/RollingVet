74562dabb9953c9cfbf2984bffda0f54657bff41
pnauj <torres.jpw@gmail.com>
2022-02-21 21:03:04 -0300

    se agrego errro404

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import Footer from "./components/layout/Footer";
 import ContactUs from "./components/views/ContactUs/ContactUs";
 import Login from "./components/layout/Login";
 import Error404 from "./components/views/Error404/Error404";
-
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PaginaPlanes from "./components/views/PaginaPlanes/PaginaPlanes";
 import AdminClientesCreate from "./components/views/AdminClientes/AdminClientesCreate";
 import AdminTurnos from "./components/views/AdminTurnos/AdminTurnos";
@@ -20,32 +15,36 @@ import ListaTurnos from "./components/views/AdminTurnos/ListaTurnos/ListaTurnos"
 import ListaClientes from "./components/views/AdminClientes/ListaClientes/ListaClientes";
 import AdminClientes from "./components/views/AdminClientes/AdminClientes";
 
-
-
 function App() {
   return (
-    
-      <BrowserRouter>
+    <BrowserRouter>
       <Navigation />
-      
+
       <main>
-       <Routes>
-      <Route exact path="/" element={<Home/>}/>
-      <Route exact path="/contactos" element={<ContactUs />}/>
-      <Route exact path="/login" element={<Login/>}/>
-      <Route exact path="/planes" element={<PaginaPlanes/>}/>
-      <Route exact path="/lista/clientes" element={<ListaClientes/>}/>
-      <Route exact path="/admin/clientes" element={<AdminClientes/>}/>
-      <Route exact path="/admin/clientes/create" element={<AdminClientesCreate/>}/>
-      <Route exact path="/lista/turnos" element={<ListaTurnos/>}/>
-      <Route exact path="/admin/turnos" element={<AdminTurnos/>}/>
-      <Route exact path="/admin/turnos/create" element={<AdminTurnosCreate/>}/>
-      <Route exact path="*" element={<Error404/>}/>
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/contactos" element={<ContactUs />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/planes" element={<PaginaPlanes />} />
+          <Route exact path="/lista/clientes" element={<ListaClientes />} />
+          <Route exact path="/admin/clientes" element={<AdminClientes />} />
+          <Route
+            exact
+            path="/admin/clientes/create"
+            element={<AdminClientesCreate />}
+          />
+          <Route exact path="/lista/turnos" element={<ListaTurnos />} />
+          <Route exact path="/admin/turnos" element={<AdminTurnos />} />
+          <Route
+            exact
+            path="/admin/turnos/create"
+            element={<AdminTurnosCreate />}
+          />
+          <Route exact path="*" element={<Error404 />} />
+        </Routes>
       </main>
       <Footer />
-      </BrowserRouter>
-
+    </BrowserRouter>
   );
 }
 
--- src/components/views/Error404/Error404.js
+++ src/components/views/Error404/Error404.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Error404.css";
 
 const Error404 = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <div className="text-center img-responsive my-5">
+        <Image src="error404.jpg" alt="Error404" width={1000}></Image>
+      </div>
+      <span>
+        <Link to="/" className="link my-3 text-center fw-bold fs-3">Volver a Rolling Code</Link>
+      </span>
+    </>
+  );
 };
 
 export default Error404;
