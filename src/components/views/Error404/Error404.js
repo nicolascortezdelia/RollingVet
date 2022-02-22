a1da41bd332dfb48021dba8ba181b8afc3f9fb39
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-02-22 08:39:59 -0300

    cambio imagen de erro404

--- src/components/views/Error404/Error404.js
+++ src/components/views/Error404/Error404.js
@@ -6,11 +6,11 @@ import "./error404.css";
 const Error404 = () => {
   return (
     <>
-      <div className="text-center img-responsive my-5">
-        <Image src="error404.jpg" alt="Error404" width={1000}></Image>
+      <div className="text-center container my-5">
+        <Image src="https://t3.ftcdn.net/jpg/02/61/08/76/360_F_261087622_8eRI0TAwDAyabS1b0Uifx1wKqHzA41r3.jpg" alt="Error404" className="img-responsive"></Image>
       </div>
       <span>
-        <Link to="/" className="link my-3 text-center fw-bold fs-3">Volver a Rolling Code</Link>
+        <Link to="/" className="link my-3 text-center fw-bold fs-3 text-dark">Volver a Rolling Code</Link>
       </span>
     </>
   );
