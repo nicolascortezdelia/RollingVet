81be708ded84c9629d83fe0ef1934ccc83b09a42
Italy76 <79777599+Italy76@users.noreply.github.com>
2022-02-23 22:07:36 -0300

    se creo componente EditClietes; se crearon states por cada input en AdminClientesCreate.js.

--- src/components/views/AdminClientes/AdminClientesCreate.js
+++ src/components/views/AdminClientes/AdminClientesCreate.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Form } from 'react-bootstrap';
 
 const AdminClientesCreate = () => {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefono, setTelefono] = useState(0);
+  const [nombreMascota, setNombreMascota] = useState("");
+  const [especie, setEspecie] = useState("");
+  const [raza, setRaza] = useState("");
+
     return (
         <div>
             <Container className="py-5">
@@ -11,33 +19,33 @@ const AdminClientesCreate = () => {
         <Form className="my-5">
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Nombre</Form.Label>
-            <Form.Control type="text" placeholder="Escriba su Nombre" />
+            <Form.Control type="text" placeholder="Escriba su Nombre" onChange={(e)=>setNombre(e.target.value)} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Apellido</Form.Label>
-            <Form.Control type="text" placeholder="Escriba su Apellido" />
+            <Form.Control type="text" placeholder="Escriba su Apellido" onChange={(e)=>setApellido(e.target.value)} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Correo Electronico</Form.Label>
-            <Form.Control type="email" placeholder="Ejemplo@correo.com" />
+            <Form.Control type="email" placeholder="Ejemplo@correo.com" onChange={(e)=>setEmail(e.value.target)} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Numero De telefono</Form.Label>
-            <Form.Control type="number" placeholder="+549XXXXXXXXXX" />
+            <Form.Control type="number" placeholder="+549XXXXXXXXXX" onChange={(e)=>setTelefono(e.target.value)} />
           </Form.Group>
           <hr />
           <h4>Informacion de la Mascotta</h4>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Nombre de la Mascota</Form.Label>
-            <Form.Control type="text" placeholder="Escriba el nombre de su mascota" />
+            <Form.Control type="text" placeholder="Escriba el nombre de su mascota" onChange={(e)=>setNombreMascota(e.target.value)} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Especie</Form.Label>
-            <Form.Control type="text" placeholder="Indique la especie de su mascota" />
+            <Form.Control type="text" placeholder="Indique la especie de su mascota" onChange={(e)=> setEspecie(e.target.value)} />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Raza</Form.Label>
-            <Form.Control type="text" placeholder="Indique la raza de su mascota" />
+            <Form.Control type="text" placeholder="Indique la raza de su mascota" onChange={(e)=>setRaza(e.target.value)} />
           </Form.Group>
           <div className="text-end">
             <button className="btn btn-success">Guardar</button>
