e308ada6e5c941ad5c665d9e63b232fed2928d2b
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-02-26 09:02:44 -0300

    modifico tipografía de componente Error

--- src/components/views/Error404/Error404.js
+++ src/components/views/Error404/Error404.js
@@ -10,7 +10,7 @@ const Error404 = () => {
         <Image src="https://t3.ftcdn.net/jpg/02/61/08/76/360_F_261087622_8eRI0TAwDAyabS1b0Uifx1wKqHzA41r3.jpg" alt="Error404" className="img-responsive"></Image>
       </div>
       <span>
-        <Link to="/" className="link my-3 text-center fw-bold fs-3 text-dark">Volver a Rolling Code</Link>
+        <Link to="/" className="link my-3 text-center fw-bold fs-3 text-danger">Volver a RollingVet</Link>
       </span>
     </>
   );
