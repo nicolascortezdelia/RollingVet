9d4664cdd2d2d7d26998fbbf9c18d05609c6bf07
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-03-01 12:14:40 -0300

    agrego carpeta validacionesTurnos

--- src/components/helpers/Validaciones.js
+++ src/components/helpers/Validaciones.js
@@ -1,13 +1,17 @@
-const regExpNombreApellido = /^[A-Za-z\s?]+$/;
-const regExpNombre = /^[A-Za-z\s?]+$/;
-const regExpFecha = /^\d{4}([\-/.])(0?[1-9]|1[1-2])\1(3[01]|[12][0-9]|0?[1-9])$/;
-const regExpHora =  /^([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/;
-const regExpTxtArea =  /^[a-zA-Z0-9?$@#()'!,+\-=_:.&€£*%\s]+$/;
+//const regExNombre = /^[A-Za-z\s?]+$/;
+const regExEmail = /^[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'+/=?^_`{|}~-]+)@(?:[a-z0-9](?:[a-z0-9-][a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+const regExTel = /^([0-9]{5})+((-{1})*)+([0-9]{6})$/;
+const regEx4Names = /^[A-Za-z\s?]+$/;
 
 
 
-export const validateNombreApellido = (field) => {
-  if (regExpNombreApellido.test(field) && field.trim() !== "") {
+export const validateNames = (campo) => {
+  if (
+    regEx4Names.test(campo) &&
+    campo.trim().length < 23 &&
+    campo.trim().length > 0 &&
+    campo !== ""
+  ) {
     return true;
   } else {
     return false;
@@ -15,34 +19,21 @@ export const validateNombreApellido = (field) => {
 };
 
 
-export const validateNombre = (field) => {
-  if (regExpNombre.test(field) && field.trim() !== "") {
-    return true;
-  } else {
-    return false;
-  }
+export const validateTel = (campo)=>{
+  if(regExTel.test(campo))
+{
+  alert("correcto");
+}else{
+  alert("Ingrese un teléfono correcto");
+}
 };
 
-export const validatetextarea = (field) => {
-  if (regExpTxtArea.test(field) && field.trim() !== "") {
-    return true;
-  } else {
-    return false;
-  }
-};
 
-export const validateHora = (field) => {
-  if (regExpHora.test(field) && field.trim() !== "") {
-    return true;
-  } else {
-    return false;
-  }
-};
 
-export const validateFecha = (field) => {
-  if (regExpFecha.test(field) && field.trim() !== "") {
-    return true;
-  } else {
-    return false;
-  }
-};
+export const validateEmail = (campo)=>{
+if(regExEmail.test(campo)){
+  return true
+}else{
+  return false
+}
+}
