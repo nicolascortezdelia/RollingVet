8c5283a5740cd5f2264dcafc7f642b5cc9128a4a
Hidalgo2002 <jehiel.hidalg@gmail.com>
2022-03-01 09:26:04 -0300

    pagina sobre nosotros casi terminada

--- src/components/Nosotros/Nosotros.js
+++ src/components/Nosotros/Nosotros.js
@@ -1,75 +1,50 @@
 import React from 'react';
 import "./Nosotros.css";
-import { Container, Row, Col,   } from 'react-bootstrap';
-
+import { Container, Row, Col,} from 'react-bootstrap';
+import Cristian from "../images/Cristian.jpeg";
+import Nicolas from "../images/Nicolas.jpeg";
+import Juan from "../images/Juan.jpeg";
+import Luciano from "../images/Luciano.jpg";
 
 const Nosotros = () => {
     return (
         <div>
             <Container>
-            <h1 className="display-2 text-center text-white p-4">Nosotros</h1>
+            <h1 className="text-danger display-5 text-center"> Sobre Nosotros</h1>
+            <br />
+            <h4 className='text-danger text-center'>
+                "Somos lo que cuidamos"
+            </h4>
             </Container>
-           <Container>
-           <h4 className="text-center text-white pb-3 px-1">
-          Nuestro amor por el vino se extiende desde los Valles Calchaquies
-            hasta la Cordillera de los Andes. <br />
-            Queremos llegar a vos a traves de este blog para que tu experiencia
-            en cada copa sea unica.
-          </h4>
-           </Container>
-          
+           <br />
           <Container>
-              <Row>
-                  <Col sm={12} md={4}>
-                  <img
-              class="opacity img img-fluid rounded-circle"
-              src="img..."
-              alt="Fotodelourdes"
-            />
-            <h4 class="text-center colorIndex">Lourdes Kasznar</h4>
+              <Row  className="justify-content-around">
+                  <Col sm={6} md={3}>
+                  <div className="text-center">
+            {" "}
+            <img src={Cristian} alt="foto de Cristian " className="rounded-circle w-100" />{" "}
+          </div>
+            <h4 class="text-center colorIndex">Cristian Brandino</h4>
                   </Col>
-                  <Col sm={12} md={4}>
-                  <img
-              class="opacity img img-fluid rounded-circle"
-              src="img..."
-              alt="Fotodenacho"
-            />
-            <h4 class="text-center colorIndex">Ignacio Vargas</h4>
-                  </Col>
-                  <Col sm={12} md={4}>
-                  <img
-              class="opacity img img-fluid rounded-circle"
-              src="img..."
-              alt="Fotodeluchy"
-            />
-            <h4 class="text-center colorIndex">Luciana Medina Escaño</h4>
-                  </Col>
-              </Row>
-           </Container>
-           <Container>
-              <Row>
-                  <Col sm={12} md={4}>
-                  <img
-              class="opacity img img-fluid rounded-circle"
-              src="../img/contactos/Nicolas.png"
-              alt="Fotodenico"
-            />
+                  <Col sm={6} md={3}>
+                  <div className="text-center">
+            {" "}
+            <img src={Nicolas} alt="foto de Nico" className="rounded-circle w-100" />{" "}
+          </div>
             <h4 class="text-center colorIndex">Nicolas Cortez D'Elia</h4>
                   </Col>
-                  <Col sm={12} md={4}>
-                  <img
-              class="opacity img img-fluid rounded-circle"
-              src="../img/contactos/Bautista.png"
-              alt="FotodeJuan"
-            />
+                  <Col sm={6} md={3}>
+                  <div className="text-center">
+            {" "}
+            <img src={Juan} alt="foto de Juan Pablo " className="rounded-circle w-100" />{" "}
+          </div>
             <h4 class="text-center ">Juan Pablo Torres</h4>
                   </Col>
-                  <Col sm={12} md={4}>
-                  <img
-              class="opacity img img-fluid rounded-circle"
-              src="../img/contactos/Luciano.png"
-              alt="Fotodeluciano"
-            />
+                  <Col sm={6} md={3}>
+                  <div className="text-center">
+            {" "}
+            <img src={Luciano} alt="foto de Luciano" className="rounded-circle w-100" />{" "}
+          </div>
             <h4 class="text-center colorIndex">Luciano Hidalgo</h4>  
                   </Col>
               </Row>
