8a2939879c6f4c7ad9f0dde65c7f56b37e166048
pnauj <torres.jpw@gmail.com>
2022-03-13 11:36:37 -0300

    d

--- src/components/views/AdminClientes/AdminClientesCreate.js
+++ src/components/views/AdminClientes/AdminClientesCreate.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
-import { Container, Form } from 'react-bootstrap';
-import {validateNames, validateTel, validateEmail} from "../../Helpers/validacionesPacientes";
+import React, { useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import {
+  validateNames,
+  validateTel,
+  validateEmail,
+} from "../../Helpers/validacionesPacientes";
 import Swal from "sweetalert2";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-const AdminClientesCreate = ({URL, getApi}) => {
+const AdminClientesCreate = ({ URL, getApi }) => {
   const [nombre, setNombre] = useState("");
   const [apellido, setApellido] = useState("");
   const [eMail, setEmail] = useState("");
@@ -13,92 +17,146 @@ const AdminClientesCreate = ({URL, getApi}) => {
   const [especie, setEspecie] = useState("");
   const [raza, setRaza] = useState("");
 
-// funion para navegar entre rutas
- const navegacion = useNavigate();
+  // funion para navegar entre rutas
+  const navegacion = useNavigate();
   //funcion para crear un objeto
-  const hundleSubmit = async(e)=>{
-    e.preventDefault()
+  const hundleSubmit = async (e) => {
+    e.preventDefault();
 
-  //validacion de los campos
-  if(!validateNames(nombre) || !validateNames(apellido) || !validateEmail(eMail) || !validateTel(telefono) || !validateNames(nombreMascota) || !validateNames(especie) || !validateNames(raza)){
-    Swal.fire("Ops!", "Algunos datos ingresados no son validos", "error")
+    //validacion de los campos
+    if (
+      !validateNames(nombre) ||
+      !validateNames(apellido) ||
+      !validateEmail(eMail) ||
+      !validateTel(telefono) ||
+      !validateNames(nombreMascota) ||
+      !validateNames(especie) ||
+      !validateNames(raza)
+    ) {
+      Swal.fire("Ops!", "Algunos datos ingresados no son validos", "error");
       // return
-  }
-  //envio de los datos para guardarlos
-  const newPaciente = {nombre, apellido, eMail, telefono, nombreMascota, especie, raza}
-  Swal.fire({
-    title: 'Estás seguro?',
-    text: "Quieres guardar los datos",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonText: 'Turno guardado!'
-  }).then(async(result) => {
-    if (result.isConfirmed) {
-      try {
-        const res = await fetch(URL, {method: "POST", headers:{"Content-Type": "application/json"}, body: JSON.stringify(newPaciente)});
-       // console.log(res);
-       if(res.status === 201){
-        Swal.fire(
-          'Creado!',
-          'Turno creado.',
-          'success'
-        );
-        getApi();
-        navegacion("/admin/clientes");
-       }
-      } catch (error) {
-        console.log(error);
-      } 
-      
     }
-  }) 
+    //envio de los datos para guardarlos
+    const newPaciente = {
+      nombre,
+      apellido,
+      eMail,
+      telefono,
+      nombreMascota,
+      especie,
+      raza,
+    };
+    Swal.fire({
+      title: "Estás seguro?",
+      text: "Quieres guardar los datos",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Turno guardado!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const res = await fetch(URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newPaciente),
+          });
+          // console.log(res);
+          if (res.status === 201) {
+            Swal.fire("Creado!", "Turno creado.", "success");
+            getApi();
+            navegacion("/admin/clientes");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    });
+  };
+  return (
+    <div>
+      <Row>
+        <Col sm={12} md={3}></Col>
+          <div><p>sadsd</p></div>
+        <Col sm={12} md={6}>
+          <Container className="py-5">
+            <h3 className="text-center text-danger display-4">
+              Agendar Pacientes
+            </h3>
+            <hr />
 
-  }
-    return (
-        <div>
-            <Container className="py-5">
-            <h3 className="text-center text-danger display-4">Agendar Pacientes</h3>
-        <hr />
-        {/* Form Product */}
-        <Form className="my-5" onSubmit={hundleSubmit}>
-        <h4>Informacion del Cliente</h4>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Nombre</Form.Label>
-            <Form.Control type="text" placeholder="Escriba su Nombre" onChange={(e)=>setNombre(e.target.value)} />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Apellido</Form.Label>
-            <Form.Control type="text" placeholder="Escriba su Apellido" onChange={(e)=>setApellido(e.target.value)} />
-          </Form.Group>
-           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Correo Electronico</Form.Label>
-            <Form.Control type="email" placeholder="ejemplo@gmail.com" onChange={(e)=>setEmail(e.target.value)} />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Numero De telefono</Form.Label>
-            <Form.Control type="number" placeholder="ej: +549 11 459977" onChange={(e)=>setTelefono(e.target.value)} />
-          </Form.Group>
-          <hr />
-          <h4>Informacion de la Mascota</h4>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Nombre de la Mascota</Form.Label>
-            <Form.Control type="text" placeholder="Escriba el nombre de su mascota" onChange={(e)=>setNombreMascota(e.target.value)} />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Especie</Form.Label>
-            <Form.Control type="text" placeholder="Indique la especie de su mascota" onChange={(e)=> setEspecie(e.target.value)} />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Raza</Form.Label>
-            <Form.Control type="text" placeholder="Indique la raza de su mascota" onChange={(e)=>setRaza(e.target.value)} />
-          </Form.Group>
-          <div className="text-end">
-            <button className="btn btn-success">Guardar</button>
-          </div>
-        </Form>
-      </Container>
-        </div>
-    );
+            <Form className="my-5" onSubmit={hundleSubmit}>
+              <h4>Informacion del Cliente</h4>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Escriba su Nombre"
+                  onChange={(e) => setNombre(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Apellido</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Escriba su Apellido"
+                  onChange={(e) => setApellido(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Correo Electronico</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="ejemplo@gmail.com"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Numero De telefono</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="ej: +549 11 459977"
+                  onChange={(e) => setTelefono(e.target.value)}
+                />
+              </Form.Group>
+              <hr />
+              <h4>Informacion de la Mascota</h4>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Nombre de la Mascota</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Escriba el nombre de su mascota"
+                  onChange={(e) => setNombreMascota(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Especie</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Indique la especie de su mascota"
+                  onChange={(e) => setEspecie(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Raza</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Indique la raza de su mascota"
+                  onChange={(e) => setRaza(e.target.value)}
+                />
+              </Form.Group>
+              <div className="text-end">
+                <button className="btn btn-success">Guardar</button>
+              </div>
+            </Form>
+          </Container>
+        </Col>
+
+        <Col sm={12} md={3}>
+        <div><p>sadsd</p></div>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default AdminClientesCreate;
