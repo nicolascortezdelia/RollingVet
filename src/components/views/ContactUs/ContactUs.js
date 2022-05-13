171a078db2a0045d6d9034ceddb7ead9f1fe80a3
pnauj <torres.jpw@gmail.com>
2022-05-13 16:17:32 -0300

    se realizaron cambios menores de estetica

--- src/components/views/ContactUs/ContactUs.js
+++ src/components/views/ContactUs/ContactUs.js
@@ -16,138 +16,146 @@ import {
 init("user_qzhExCW0FgIpI81KTZIIe");
 
 const ContactUs = () => {
+  // States
 
- // States
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mesage, setMesage] = useState("");
 
- const [name, setName] = useState("");
- const [email, setEmail] = useState("");
- const [mesage, setMesage] = useState("");
+  // EmailJs
+  const form = useRef();
 
- // EmailJs
- const form = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
- const handleSubmit = (e) => {
-   e.preventDefault();
+    // validamos datos
+    if (
+      !validateNames(name) ||
+      !validateEmail(email) ||
+      !validateMesage(mesage)
+    ) {
+      Swal.fire({
+        icon: "error",
+        title: "Ay!",
+        text: "Ingresaste algun dato incorrecto, por favor revisá el formulario",
+      });
+    } else {
+      // Email js
+      emailjs
+        .sendForm("service_dihrpp7", "template_qe3arwq", form.current)
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      setName("");
+      setEmail("");
+      setMesage("");
 
-   // validamos datos
-   if (!validateNames(name) || !validateEmail(email) || !validateMesage(mesage)) {
-     Swal.fire({
-       icon: "error",
-       title: "Ay!",
-       text: "Ingresaste algun dato incorrecto, por favor revisá el formulario",
-     });
-   } else {
-     // Email js
-     emailjs.sendForm("service_dihrpp7", "template_qe3arwq", form.current)
-       .then(
-         (result) => {
-           console.log(result.text);
-         },
-         (error) => {
-           console.log(error.text);
-         }
-       );
-     setName("");
-     setEmail("");
-     setMesage("");
+      Swal.fire({
+        icon: "success",
+        title: "Listo!",
+        text: "Gracias por tu consulta, pronto nos pondremos en contacto con vos",
+      });
+    }
+  };
 
-     Swal.fire({
-       icon: "success",
-       title: "Listo!",
-       text: "Gracias por tu consulta, pronto nos pondremos en contacto con vos",
-     });
-   }
- };
-
- 
   return (
     <>
-    <Fade bottom>
-    <div className="container-fluid p-1" >
-      <div className="p-2 text-center">
-      <h3 className="text-danger display-4  ">Contactanos</h3>
-      <hr className="text-dark" />
+      <Fade bottom>
+        <div className="container-fluid p-1">
+          <div className="p-2 text-center">
+            <h3 className="text-danger display-4 ">Contactanos</h3>
+            <hr className="text-dark" />
+          </div>
 
-    </div>
+          <Container>
+            <Card className="mb-3">
+              <Row>
+                <Col sm={12} md={6} className="border-end border-danger">
+                  <Form
+                    className="my-4 container text-start "
+                    onSubmit={handleSubmit}
+                    ref={form}
+                  >
+                    <Form.Group
+                      className="mb-3"
+                      controlId="exampleForm.ControlInput1"
+                      id="nombre"
+                    >
+                      <Form.Label className="fw-bold text-dark">
+                        Nombre y Apellido
+                      </Form.Label>
+                      <input
+                        value={name}
+                        name="user_name"
+                        className="form-control"
+                        type="text"
+                        placeholder="Ej: Andrea Pérez"
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </Form.Group>
+                    <Form.Group
+                      className="mb-3"
+                      controlId="exampleForm.ControlInput1"
+                    >
+                      <Form.Label className="fw-bold text-dark">
+                        Correo Electronico
+                      </Form.Label>
+                      <input
+                        value={email}
+                        className="form-control"
+                        type="email"
+                        placeholder="ejemplo@gmail.com"
+                        name="user_email"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <Form.Text className="text-muted">
+                        No compartiremos tu e-mail o tus datos con nadie más
+                      </Form.Text>
+                    </Form.Group>
 
-        <Container  >
-         
-          <Card className="mb-3">
-          <Row>
-            <Col sm={12} md={6} className="border-end border-danger" >
-            <Form className="my-4 container text-start "   onSubmit={handleSubmit} ref={form} >
-                <Form.Group
-                  className="mb-3"
-                  controlId="exampleForm.ControlInput1" id="nombre"
-                >
-                  <Form.Label className="fw-bold text-dark">
-                    Nombre y Apellido
-                  </Form.Label>
-                  <input
-                    value={name}
-                    name="user_name"
-                    className="form-control"
-                    type="text"
-                    placeholder="Ej: Andrea Pérez"
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                  </Form.Group>
-                <Form.Group
-                  className="mb-3"
-                  controlId="exampleForm.ControlInput1"
-                >
-                  <Form.Label className="fw-bold text-dark">
-                    Correo Electronico
-                  </Form.Label>
-                  <input
-                    value={email}
-                    className="form-control"
-                    type="email"
-                    placeholder="ejemplo@gmail.com"
-                    name="user_email"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <Form.Text className="text-muted">
-                    No compartiremos tu e-mail o tus datos con nadie más
-                  </Form.Text>
-                </Form.Group>
+                    <Form.Group
+                      className="mb-3"
+                      controlId="exampleForm.ControlTextarea1"
+                    >
+                      <Form.Label className="fw-bold text-dark">
+                        Dejanos tu Consulta
+                      </Form.Label>
+                      <Form.Control
+                        value={mesage}
+                        as="textarea"
+                        rows={3}
+                        placeholder="Ingrese su consulta"
+                        onChange={(e) => setMesage(e.target.value)}
+                      />
+                    </Form.Group>
+                    <Button
+                      className="btn btn-danger rounded-pill"
+                      type="submit"
+                    >
+                      Enviar
+                    </Button>
+                  </Form>
+                </Col>
 
-
-                <Form.Group
-                  className="mb-3"
-                  controlId="exampleForm.ControlTextarea1"
-                >
-                 <Form.Label className="fw-bold text-dark">
-                    Dejanos tu Consulta
-                  </Form.Label>
-                  <Form.Control
-                    value={mesage} 
-                    as="textarea"
-                    rows={3}
-                    placeholder="Ingrese su consulta"
-                    onChange={(e) => setMesage(e.target.value)}
-                  />
-                </Form.Group>
-                <Button className="btn btn-danger rounded-pill" type="submit">
-                  Enviar
-                </Button>
-              </Form>
-            </Col>
-
-            <Col sm={12} md={6}>
-              <Iframe className="my-4 container"
-
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.1023707775184!2d-65.20939048529601!3d-26.83669609650096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses!2sar!4v1644714185703!5m2!1ses!2sar"
-                width="600"
-                height="450"
-                style="border:0;"
-                allowfullscreen=""
-                loading="lazy"
-              ></Iframe>
-            </Col>
-          </Row>
-          </Card>
-        </Container>
+                <Col sm={12} md={6}>
+                  <Iframe
+                    className="my-4 container"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.1023707775184!2d-65.20939048529601!3d-26.83669609650096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses!2sar!4v1644714185703!5m2!1ses!2sar"
+                    width="600"
+                    height="450"
+                    style="border:0;"
+                    allowfullscreen=""
+                    loading="lazy"
+                  ></Iframe>
+                </Col>
+              </Row>
+            </Card>
+          </Container>
         </div>
       </Fade>
     </>
